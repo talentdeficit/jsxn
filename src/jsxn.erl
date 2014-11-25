a622f895da61ddebeea24b3483e68a4b533810f2
Parnell Springmeyer <parnell@ixmat.us>
2014-11-25 16:10:56 -0600

    Derp! Gotta remove that gunk

--- src/jsxn.erl
+++ src/jsxn.erl
@@ -163,7 +163,7 @@ handle_event(end_object, State) -> finish(State);
 handle_event(start_array, State) -> start_array(State);
 handle_event(end_array, State) -> finish(State);
 
-handle_event({key, Key}, {What, Config} = State) -> io:format("~p", [What]),insert(format_key(Key, Config), State);
+handle_event({key, Key}, {_, Config} = State) -> insert(format_key(Key, Config), State);
 
 handle_event({_, Event}, State) -> insert(Event, State).
 
